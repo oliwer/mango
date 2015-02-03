8ed686faa7c961e7a9e62ff5d4e51a325312776a
Olivier Duclos <odc@cpan.org>
2015-02-03 20:01:41 +0100

    fix github url in the Makefile

--- lib/Mango.pm
+++ lib/Mango.pm
@@ -24,7 +24,7 @@ has [qw(max_write_batch_size wtimeout)] => 1000;
 has protocol => sub { Mango::Protocol->new };
 has w => 1;
 
-our $VERSION = '1.15';
+our $VERSION = '1.16';
 
 sub DESTROY { shift->_cleanup }
 
